cb1f138784a42e6255551d22a6559b9c5661d637
Phat <phatdroid94@gmail.com>
2022-06-21 19:21:34 +0700

    save file and get file internal storage

--- app/src/main/java/com/example/todolistsound29032022/presentation/MainActivity.java
+++ app/src/main/java/com/example/todolistsound29032022/presentation/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.todolistsound29032022.presentation;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
@@ -10,15 +14,20 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.todolistsound29032022.R;
 import com.example.todolistsound29032022.data.model.Todo;
 import com.example.todolistsound29032022.presentation.viewmodel.MainViewModel;
+import com.example.todolistsound29032022.utils.FileUtil;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     MainViewModel mainViewModel;
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img = findViewById(R.id.imageView);
 
         mainViewModel = new ViewModelProvider(this, new ViewModelProvider.Factory() {
             @NonNull
@@ -43,5 +52,17 @@ public class MainActivity extends AppCompatActivity {
         });
 
         mainViewModel.queryTodoLists();
+
+
+//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.asynchornous);
+//        String nameImage = String.format("image%d",System.currentTimeMillis());
+//        String path = FileUtil.saveToInternalStorage(bitmap, this, nameImage);
+//        Log.d("BBB", path);
+        // /data/data/com.example.todolistsound29032022/app_assets/image1655813701493.png
+
+        Bitmap bitmap = FileUtil.loadImageFromStorage("/data/data/com.example.todolistsound29032022/app_assets/image1655813701493.png");
+        img.setImageBitmap(bitmap);
     }
+
+
 }
---
+++ app/src/main/java/com/example/todolistsound29032022/utils/FileUtil.java
@@ -0,0 +1,49 @@
+package com.example.todolistsound29032022.utils;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileUtil {
+    public static String saveToInternalStorage(Bitmap bitmapImage, Context context, String nameFile) {
+        ContextWrapper cw = new ContextWrapper(context);
+        // path to /data/data/yourapp/app_data/assets
+        File directory = cw.getDir("assets", Context.MODE_PRIVATE);
+        // Create assets
+        File myPath = new File(directory, nameFile + ".png");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(myPath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return directory.getAbsolutePath();
+    }
+
+    public static Bitmap loadImageFromStorage(String path) {
+        try {
+            File file = new File(path);
+            Bitmap bitmap = BitmapFactory.decodeStream(new FileInputStream(file));
+            return bitmap;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
