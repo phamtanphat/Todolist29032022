52ca47bc88b6495ac2c4fcb4ad8d1524af3221de
Phat <phatdroid94@gmail.com>
2022-06-21 18:04:00 +0700

    database

---
+++ app/src/main/java/com/example/todolistsound29032022/data/datasource/local/TodoDao.java
@@ -0,0 +1,7 @@
+package com.example.todolistsound29032022.data.datasource.local;
+
+import androidx.room.Dao;
+
+@Dao
+public interface TodoDao {
+}
--- app/src/main/java/com/example/todolistsound29032022/data/datasource/local/TodoDatabase.java
+++ app/src/main/java/com/example/todolistsound29032022/data/datasource/local/TodoDatabase.java
@@ -1,4 +1,26 @@
 package com.example.todolistsound29032022.data.datasource.local;
 
-public class TodoDatabase {
+import android.app.Application;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.todolistsound29032022.data.datasource.local.entities.TodoEntity;
+
+@Database(entities = TodoEntity.class, version = 1)
+public abstract class TodoDatabase extends RoomDatabase {
+    private static TodoDatabase instance;
+    public abstract TodoDao todoDao();
+
+    public static TodoDatabase getInstance(Application context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(
+                    context,
+                    TodoDatabase.class,
+                    "todo_database.sql"
+            ).build();
+        }
+        return instance;
+    }
 }
